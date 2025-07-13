661082fda1671a5005f434093d5ed349c9b8e793
satriaaao <lordeoid0@gmail.com>
2025-07-14 03:19:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ document.getElementById("cekForm").addEventListener("submit", function (e) {
     e.preventDefault();
 
     const nik = document.getElementById("nik").value.trim();
-    const nama = document.getElementById("nama").value.trim();
-    const tgl_lahir = document.getElementById("tgl_lahir").value;
 
     if (!nik || !nama || !tgl_lahir) {
         alert("Lengkapi semua data dulu!");
